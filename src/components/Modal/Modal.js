ac6eef2cbf5693cdeb822c2c5e279b67018b6b18
taisiia-filikova <filikova@gmail.com>
2020-12-27 23:18:09 +0200

    Переделывает  в модалке классы в хуки

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,52 +1,38 @@
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
 
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+function Modal({ largeImageURL, onToggleModal }) {
+  useEffect(() => {
+    const handleKeyDown = evt => {
+      if (evt.code === 'Escape') {
+        onToggleModal();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onToggleModal]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onToggleModal();
+  const handleBackdropClick = evt => {
+    if (evt.currentTarget === evt.target) {
+      onToggleModal();
     }
   };
 
-  handleBackdropClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.onToggleModal();
-    }
-  };
-
-  render() {
-    const { largeImageURL } = this.props;
-
-    return createPortal(
-      <div className={s.backdrop} onClick={this.handleBackdropClick}>
-        <div className={s.modal}>
-          <img src={largeImageURL} alt="" />
-        </div>
-      </div>,
-      modalRoot,
-    );
-  }
-}
-
-{
-  /* <div className="Overlay">
-  <div className="Modal">
-    <img src="" alt="" />
-  </div>
-</div> */
+  return createPortal(
+    <div className={s.backdrop} onClick={handleBackdropClick}>
+      <div className={s.modal}>
+        <img src= {largeImageURL} alt= ''/>
+      </div>
+    </div>,
+    modalRoot,
+  );
 }
 
 Modal.propTypes = {
